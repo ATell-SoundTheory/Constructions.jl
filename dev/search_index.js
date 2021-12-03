3fcd00be0818d6aa43deb226d7e6ea2f6584fd47
Documenter.jl <documenter@juliadocs.github.io>
2021-12-03 11:28:02 +0000

    build based on 9fdc868

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Constructions","category":"page"},{"location":"#Constructions","page":"Home","title":"Constructions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Constructions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Constructions]","category":"page"}]
+}
